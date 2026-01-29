d4a7ef4b2a4b41497290e2cb42248551ce932c86
tonalmathewfegno <162094338+tonalmathewfegno@users.noreply.github.com>
2026-01-29 10:50:58 +0530

    chore: Remove static export, base path, asset prefix, and unoptimized image configurations from Next.js.

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: 'export', // 👈 REQUIRED
-  basePath: '/abc-spacery',
-  assetPrefix: '/abc-spacery/',
-  images: {
-    unoptimized: true, // GitHub Pages requirement
-  },
 };
 
 export default nextConfig;
